7f9e9b4ba4a214fb9edfd81df0a571ab5f683e64
tenorionique <monique_doretto@hotmail.com>
2021-06-10 17:59:22 -0300

    adicionando o html feito, o css quase terminado e a cifra de cesar funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,37 @@
 const cipher = {
-  // ...
-};
-
-export default cipher;
+  
+  encode: function (chaveDesloc, string) {
+   let newEntrada = string.toUpperCase();
+   let cifraMsg = "";
+ 
+   for (let i = 0; i < newEntrada.length; i++) {
+ 
+     let letterAsc = newEntrada[i].charCodeAt();
+     let encodeForm = ((letterAsc - 65 + chaveDesloc) % 26) + 65;
+ 
+     cifraMsg += String.fromCharCode(encodeForm);
+ 
+ 
+   }
+   return cifraMsg;
+ 
+  },
+  decode: function(chaveDesloc, string) {
+    let novaEntrada = string.toUpperCase();
+    let decifrarMsg = "";
+ 
+    for (let i=0; i <novaEntrada.length; i++) {
+ 
+     let letterAsc = novaEntrada[i].charCodeAt();
+     let decodForm = ((letterAsc - 90 - chaveDesloc)%26) +90;
+ 
+     decifrarMsg += String.fromCharCode(decodForm);
+ 
+    }
+    return decifrarMsg;
+ 
+  }
+ 
+ };
+ 
+ export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+let chaveDesloc;
+let codificar;
+let result;
+let decodificar;
+let buttonCif = window.document.getElementById('butCifrar')
+buttonCif.addEventListener('click', cifrar)
+let buttonDecif = window.document.getElementById('butDecifrar')
+buttonDecif.addEventListener('click', decifrar)
+
+function cifrar() {
+    chaveDesloc = Number(window.document.getElementById('desloc').value)
+    codificar =  window.document.getElementById('text1').value
+    result = window.document.getElementById('result')
+    //result.innerHTML = `Testando vai aparecer o texto codificado mas por enquanto ${chaveDesloc} e ${codificar}` 
+    result.innerHTML = cipher.encode(chaveDesloc, codificar)
+   }
+
+function decifrar() {
+    chaveDesloc = Number(window.document.getElementById('desloc').value)
+     decodificar = window.document.getElementById('textodois').value
+     result = window.document.getElementById('result')
+   // result.innerHTML = `Testando vai aparecer o texto decodificado mas por enquanto ${chaveDesloc} e ${decodificar}`
+    result.innerHTML = cipher.decode(chaveDesloc, decodificar)
+}   
+   //no result.innerhtml deixei esse teste mas é para linkar com o cipher para aparecer a 
+   //mensagem criptografada na textarea result
