9631e71d880ea1fd39f02959902d8523ada53dd0
tenorionique <monique_doretto@hotmail.com>
2021-10-26 19:26:42 -0300

    fix(css/style) ajusta responsividade e margens

--- README.md
+++ README.md
@@ -114,3 +114,59 @@ No rodapé da página, pode ser encontrado um link que explica o que é cifra de
 * [ :white_check_mark:] Interface que permita escrever um texto para ser decifrado.
 * [ :white_check_mark:] Interface que mostre o resultado decifrado corretamente.
 * [ :white_check_mark:] Cifrar/decifrar minúsculas.
+
+# Jogos Olímpicos do Rio de Janeiro
+
+
+## Índice
+
+* [1. Introdução](#1-Introdução)
+* [2. Definição de Produto](#2-Definição-de-Produto)
+* [3. Definição do Usuário](#3-Definição-do-Usuário)
+* [4. Histórias de Usuarios](#4-Histórias-de-Usuarios)
+* [5. Protótipo de Baixa Fidelidade](#5-Protótipo-de-Baixa-Fidelidade)
+* [6. Teste de Usabilidade](#6-Teste-de-Usabilidade)
+* [7. Funcionalidades da Aplicação](#7-Funcionalidades-da-Aplicação)
+* [8. Considerações Tecnicas e Execução](#8-Considerações-Tecnicas-e-Execução)
+* [9. Autores](#9-Autores)
+
+***
+
+## 1. Introdução :black_nib:
+
+
+
+## 2. Definição de Produto :pencil: 
+
+
+
+
+
+## 3. Definição do Usuário :family: :alien:
+
+ 
+
+
+
+## 4. Histórias de Usuarios  :book: :floppy_disk:
+
+
+
+
+
+## 5. Protótipo de Baixa Fidelidade: 
+
+
+
+
+
+## 6. Teste de Usabilidade: :mag_right: :unlock: :lock:
+
+
+
+## 7. Funcionalidades da Aplicação: :mag_right: :unlock: :lock:
+
+## 8. Considerações Tecnicas e Execução
+
+## 9. Autores
+
--- src/cipher.js
+++ src/cipher.js
@@ -1,67 +1,68 @@
 const cipher = {
-  
-  encode: function Criptografar (chaveDesloc, string) {
+  encode: function Criptografar(chaveDesloc, string) {
     const chaveVazia = !chaveDesloc;
     const chaveNotNumber = typeof chaveDesloc !== "number";
-    if (chaveVazia || chaveNotNumber){
-      throw new TypeError;
-    } //retorna um erro caso o usuario digite algo que não seja um numero na chave de deslocamento
+    if (chaveVazia || chaveNotNumber) {
+      throw new TypeError();
+    } 
 
-   let newEntrada = string;
-   let cifraMsg = "";
-   let encodeForm = "";
- 
-   for (let i = 0; i < newEntrada.length; i++) {
- // faz um loop para transformar a string, pegando uma letra por vez e transformando em um codigo da tabela asc
- 
- 
-     let letterAsc = newEntrada.charCodeAt(i);
-     if ( letterAsc >= 65 && letterAsc <= 90) {
-       encodeForm += String.fromCharCode(((letterAsc - 65 + chaveDesloc) % 26) + 65) 
-       
+    let newEntrada = string;
+    let cifraMsg = "";
+    let encodeForm = "";
 
-     } else if (letterAsc >= 97 && letterAsc <= 122) {
-        encodeForm += String.fromCharCode(((letterAsc - 97 + chaveDesloc) % 26) + 97) 
-        
-//depois verificando por meio de if else, quais são maiusculas e minisculas, ignorando espaço e ponto
-     } else {
-       encodeForm += newEntrada.charAt(i)
-     }
-     cifraMsg = encodeForm;
-   }
-//transforma o codigo da asc em letra outra vez e retorna uma mensagem cifrada para a caixa de texto com função e dom
-   return cifraMsg;
- 
+    for (let i = 0; i < newEntrada.length; i++) {
+
+      let letterAsc = newEntrada.charCodeAt(i);
+      if (letterAsc >= 65 && letterAsc <= 90) {
+        encodeForm += String.fromCharCode(
+          ((letterAsc - 65 + chaveDesloc) % 26) + 65
+        );
+      } else if (letterAsc >= 97 && letterAsc <= 122) {
+        encodeForm += String.fromCharCode(
+          ((letterAsc - 97 + chaveDesloc) % 26) + 97
+        );
+
+      } else {
+        encodeForm += newEntrada.charAt(i);
+      }
+      cifraMsg = encodeForm;
+    }
+    return cifraMsg;
   },
-  decode: function Descriptografar (chaveDesloc, string) {
 
+  decode: function Descriptografar(chaveDesloc, string) {
     const chaveVazia = !chaveDesloc;
     const chaveNotNumber = typeof chaveDesloc !== "number";
-    if (chaveVazia || chaveNotNumber){
-    throw new TypeError; }
+    if (chaveVazia || chaveNotNumber) {
+      throw new TypeError();
+    }
 
     let novaEntrada = string;
     let decifrarMsg = "";
     let decodForm = "";
 
-    for (let i=0; i <novaEntrada.length; i++) {
- 
-     let letterAsc = novaEntrada.charCodeAt(i);
-     if ( letterAsc >= 65 && letterAsc <= 90) {
-       decodForm += String.fromCharCode(((letterAsc - 90 - chaveDesloc)%26) +90) 
-       
-     } else if (letterAsc >= 97 && letterAsc <= 122){
-       decodForm += String.fromCharCode(((letterAsc - 122 - chaveDesloc)%26) +122) 
-       
-     } else {
-      decodForm += novaEntrada.charAt(i)
-     }
-     decifrarMsg = decodForm;
+    for (let i = 0; i < novaEntrada.length; i++) {
+
+      let letterAsc = novaEntrada.charCodeAt(i);
+
+      if (letterAsc >= 65 && letterAsc <= 90) {
+        decodForm += String.fromCharCode(
+          ((letterAsc - 90 - chaveDesloc) % 26) + 90
+        );
+
+      } else if (letterAsc >= 97 && letterAsc <= 122) {
+        decodForm += String.fromCharCode(
+          ((letterAsc - 122 - chaveDesloc) % 26) + 122
+        );
+      } else {
+        decodForm += novaEntrada.charAt(i);
+      }
+
+      decifrarMsg = decodForm;
     }
+    
     return decifrarMsg;
- 
-  }
- 
- };
- 
- export default cipher;
+  },
+};
+
+export default cipher;
