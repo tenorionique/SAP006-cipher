0d503f0bd1756708b8e8fcf659dd9e3a0f4acfef
tenorionique <monique_doretto@hotmail.com>
2021-10-26 21:12:30 -0300

    Updates

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-//console.log(cipher);
 let chaveDesloc;
 let codificar;
 let result;
 let decodificar;
-let buttonCif = window.document.getElementById('butCifrar')
-buttonCif.addEventListener('click', cifrar)
-let buttonDecif = window.document.getElementById('butDecifrar')
-buttonDecif.addEventListener('click', decifrar)
+let buttonCif = window.document.getElementById("butCifrar");
+buttonCif.addEventListener("click", cifrar);
+let buttonDecif = window.document.getElementById("butDecifrar");
+buttonDecif.addEventListener("click", decifrar);
 
 function cifrar() {
-    chaveDesloc = Number(window.document.getElementById('desloc').value)
-    codificar =  window.document.getElementById('text1').value
-    result = window.document.getElementById('result')
-   
-    result.innerHTML = cipher.encode(chaveDesloc, codificar)
-   }
-//elementos DOM trazendo o que o usuario digitou e retornando o resultado cifrado e decifrado
+  chaveDesloc = Number(window.document.getElementById("desloc").value);
+  codificar = window.document.getElementById("text1").value;
+  result = window.document.getElementById("result");
+
+  result.innerHTML = cipher.encode(chaveDesloc, codificar);
+}
+
 function decifrar() {
-    chaveDesloc = Number(window.document.getElementById('desloc').value)
-     decodificar = window.document.getElementById('textodois').value
-     result = window.document.getElementById('result')
-   
-    result.innerHTML = cipher.decode(chaveDesloc, decodificar)
-}   
-   
+  chaveDesloc = Number(window.document.getElementById("desloc").value);
+  decodificar = window.document.getElementById("textodois").value;
+  result = window.document.getElementById("result");
+
+  result.innerHTML = cipher.decode(chaveDesloc, decodificar);
+}
